c40f37c4c56ee6155486bc19eda4d60395089d3a
yishnn710476 <161700467+yishnn710476@users.noreply.github.com>
2024-07-04 21:20:54 +0800

    Add gpt-voice service

---
+++ VSCodeExtension/repo-code-assistant/src/services/Voice/gptAPIService.ts
@@ -0,0 +1,67 @@
+import * as vscode from 'vscode';
+import OpenAI from 'openai';
+import fs from 'fs';
+import path from 'path';
+import SettingsManager from '../../api/settingsManager';
+import { AbstractVoiceService } from './abstractVoiceService';
+
+export class gptAPIService extends AbstractVoiceService {
+  private apiKey: string;
+  private readonly settingsListener: vscode.Disposable;
+
+  constructor(
+    context: vscode.ExtensionContext,
+    settingsManager: SettingsManager,
+  ) {
+    super(
+    );
+    this.apiKey = settingsManager.get('openaiApiKey');
+
+    // Listen for settings changes
+    this.settingsListener = vscode.workspace.onDidChangeConfiguration((e) => {
+      if (e.affectsConfiguration('repo-code-assistant.openAiApiKey')) {
+        this.apiKey = settingsManager.get('openaiApiKey');
+      }
+    });
+    context.subscriptions.push(this.settingsListener);
+  }
+    public async TextToSpeech(generatedText:string){
+      const openai = new OpenAI({
+        apiKey: this.apiKey,
+      });
+        try {
+        const mp3 = await openai.audio.speech.create({
+          model: "tts-1",
+          voice: "nova",
+          input: generatedText,
+        });
+        const buffer = Buffer.from(await mp3.arrayBuffer());
+
+        const mediaPath = path.join(__dirname, '..', 'dist', 'media');
+        // Define the file name
+
+        const mp3FilePath = path.join(mediaPath, 'generated.mp3');
+        // Write the MP3 file to the specified path
+        fs.writeFileSync(mp3FilePath, buffer);
+        return mp3FilePath;
+      }catch(err){
+        return 'Failed to Voice to Text.';
+      }
+    }
+
+    public async SpeechToText(speechFile:string) {
+      const openai = new OpenAI({
+        apiKey: this.apiKey,
+      });
+      try {
+        const transcription = await openai.audio.transcriptions.create({
+          file: fs.createReadStream(speechFile),
+          model: "whisper-1",
+          response_format: "text",
+        });
+        return transcription.text;
+      }catch(err){
+        return 'Failed to Speech to Text.';
+      }    
+  }
+}
